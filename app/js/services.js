25af75e56560b1fbdfa7e833c6e80b1b718a9c2e
Karl Krissian <krissian@dis.ulpgc.es>
2015-09-15 19:53:51 +0200

    added use of Blob module

--- app/js/services.js
+++ app/js/services.js
@@ -10,3 +10,11 @@ IPOLDemoServices.factory('Demo', ['$resource',
       query: {method:'GET', params:{demoId:'demos'}, isArray:true}
     });
   }]);
+
+IPOLDemoServices.factory('DemoBlobs', ['$resource',
+  function($resource){
+    return $resource('http://localhost:7777/get_blobs_of_demo_by_name_ws?demo_name=:demoId', {}, {
+      query: {method:'JSONP', params:{demoId:'demos'}, isArray:true}
+    });
+  }]);
+
